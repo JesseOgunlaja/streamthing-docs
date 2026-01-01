3f7a7b0deec01ec07afc7e8b4b6058e4cf3701df
JesseOgunlaja <jesseogunlaja@gmail.com>
2026-01-01 15:26:27 +0000

    fix: remove prettier-plugin-sh

--- components/code/CodeBlock.tsx
+++ components/code/CodeBlock.tsx
@@ -2,57 +2,64 @@ import esTree from "prettier/plugins/estree";
 import html from "prettier/plugins/html";
 import ts from "prettier/plugins/typescript";
 import prettier from "prettier/standalone";
-import sh from "prettier-plugin-sh";
-import { codeToHtml } from "shiki";
+// import sh from "prettier-plugin-sh";
 import styles from "@/styles/page.module.css";
+import { codeToHtml } from "shiki";
 import CodeBlockContent from "./CodeBlockContent";
 import CopyCodeBlock from "./CopyCodeBlock";
 
 interface PropsType {
-	code: string;
-	lang?: string;
-	fileName?: string;
+    code: string;
+    lang?: string;
+    fileName?: string;
 }
 
-const languagesToFormat = [
-	"jsx",
-	"tsx",
-	"javascript",
-	"typescript",
-	"dotenv",
-	"html",
-];
+const languagesToFormat = ["jsx", "tsx", "javascript", "typescript", "html"];
+
+function dedent(code: string) {
+    const lines = code.replace(/\t/g, "  ").split("\n");
+
+    const indents = lines
+        .filter((line) => line.trim().length > 0)
+        .map((line) => line.match(/^ */)![0].length);
+
+    const minIndent = Math.min(...indents, Infinity);
+
+    return lines
+        .map((line) => line.slice(minIndent))
+        .join("\n")
+        .trimEnd();
+}
 
 const CodeBlock = async ({
-	code,
-	lang = "javascript",
-	fileName,
+    code,
+    lang = "javascript",
+    fileName,
 }: PropsType) => {
-	const formattedCode = languagesToFormat.includes(lang)
-		? await prettier.format(code, {
-				parser:
-					lang === "dotenv" ? "sh" : lang === "html" ? "html" : "typescript",
-				plugins: [esTree, sh, ts, html],
-			})
-		: code;
+    const formattedCode = languagesToFormat.includes(lang)
+        ? await prettier.format(code, {
+              parser: lang === "html" ? "html" : "typescript",
+              plugins: [esTree, ts, html],
+          })
+        : dedent(code);
 
-	const lightHtml = await codeToHtml(formattedCode.trim(), {
-		lang: lang,
-		theme: `github-light`,
-	});
+    const lightHtml = await codeToHtml(formattedCode.trim(), {
+        lang: lang,
+        theme: `github-light`,
+    });
 
-	const darkHtml = await codeToHtml(formattedCode.trim(), {
-		lang: lang,
-		theme: `github-dark`,
-	});
+    const darkHtml = await codeToHtml(formattedCode.trim(), {
+        lang: lang,
+        theme: `github-dark`,
+    });
 
-	return (
-		<div className={styles.code}>
-			{fileName && <p className={styles.fileName}>{fileName}</p>}
-			<CodeBlockContent lightHtml={lightHtml} darkHtml={darkHtml} />
-			<CopyCodeBlock formattedCode={code} />
-		</div>
-	);
+    return (
+        <div className={styles.code}>
+            {fileName && <p className={styles.fileName}>{fileName}</p>}
+            <CodeBlockContent lightHtml={lightHtml} darkHtml={darkHtml} />
+            <CopyCodeBlock formattedCode={code} />
+        </div>
+    );
 };
 
 export default CodeBlock;
